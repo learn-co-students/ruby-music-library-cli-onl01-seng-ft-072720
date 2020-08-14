01b872812cd0db2e94321624f3f913d84ddce528
Anthony Lepore <violacellonyc@gmail.com>
2020-08-14 20:59:08 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,3 +1,15 @@
 module Concerns::Findable
   # Module code here
+  
+  
+   def find_by_name(name)
+  result = nil 
+  self.class.each do |i|
+    result = i if i.name == name 
+  end
+  result
+end
+  
+  
+  
 end
