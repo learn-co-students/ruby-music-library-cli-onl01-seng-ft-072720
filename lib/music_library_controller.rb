9187276c00ffb06c683626d2a52ac32f8d4e4279
Anthony Lepore <violacellonyc@gmail.com>
2020-08-16 00:47:50 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -24,8 +24,34 @@ class MusicLibraryController
     input = gets.strip
     if input == "exit"
       i = 1 
-    else
-      i = 0 
+    elsif
+   input == "list songs"
+        list_songs
+        elsif
+        input == "list artists"
+        list_artists
+        
+        elsif
+   input == "list genres"
+        list_genres
+        elsif
+        input == "list artist"
+        list_songs_by_artist
+        
+        elsif
+  
+        input == "list genre"
+        list_songs_by_genre
+        
+        elsif
+   input == "play song"
+      play_song
+      
+        
+        
+        
+        
+        
     end  #ends If 
   end # ends While 
 end # ends call 
