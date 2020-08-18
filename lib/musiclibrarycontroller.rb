5d012a77ef8a4f10bc98ec12c9c27595e925cf1e
VirgilCantu <cantu.sampsom@gmail.com>
2020-08-18 12:32:52 -0500

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -12,15 +12,15 @@ class MusicLibraryController
         input = ""
 
         while input != "exit"
-        puts "Welcome to your music library!"
-        puts "To list all of your songs, enter 'list songs'."
-        puts "To list all of the artists in your library, enter 'list artists'."
-        puts "To list all of the genres in your library, enter 'list genres'."
-        puts "To list all of the songs by a particular artist, enter 'list artist'."
-        puts "To list all of the songs of a particular genre, enter 'list genre'."
-        puts "To play a song, enter 'play song'."
-        puts "To quit, type 'exit'."
-        puts "What would you like to do?"
+            puts "Welcome to your music library!"
+            puts "To list all of your songs, enter 'list songs'."
+            puts "To list all of the artists in your library, enter 'list artists'."
+            puts "To list all of the genres in your library, enter 'list genres'."
+            puts "To list all of the songs by a particular artist, enter 'list artist'."
+            puts "To list all of the songs of a particular genre, enter 'list genre'."
+            puts "To play a song, enter 'play song'."
+            puts "To quit, type 'exit'."
+            puts "What would you like to do?"
 
         input = gets.strip
 
@@ -43,7 +43,7 @@ class MusicLibraryController
         
     def list_songs
         songs_list = Song.all.sort{|a,b| a.name <=> b.name}
-        songs_list = songs_list.uniq
+        # songs_list = songs_list.uniq
         songs_list.each_with_index do |song, i|
         puts "#{i+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
         end
